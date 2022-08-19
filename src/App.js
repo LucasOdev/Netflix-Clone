1cb8a76a1568a783a5d27c4c8d78b0d4cfaf1802
unknown <lucas.olisboa@outlook.com>
2022-08-19 18:14:26 -0300

    finalização do projeto com a criação do header, carregamento, responsividade e filme em destaque.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Tmdb from "./Tmdb";
 import MovieRow from "./components/MovieRow";
 import './App.css';
 import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [FeaturedData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false)
   useEffect (() => {
     const loadAll = async () => {
       /*pegando as listas */
@@ -21,14 +23,31 @@ export default () => {
       /* pegar filme escolhido*/
       let chosen = originals[0].items.results[randomChosen];
       let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
-
-      console.log(chosenInfo)
+      setFeaturedData(chosenInfo)
+      
       
     }
     loadAll();
   }, [])
+
+    /*manipulando o header */
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+    window.addEventListener('scroll', scrollListener);
+      return () => {
+        window.removeEventListener('scroll', scrollListener);
+      }
+  },[])
   return (
     <div className="page">
+
+      <Header black={blackHeader}/>
       { FeaturedData &&
       <FeaturedMovie item={FeaturedData}/>}
       <section className="lists">
@@ -36,6 +55,16 @@ export default () => {
           <MovieRow key={key} title={item.title} items={item.items}/>
         ))}
       </section>
+      <footer>
+        Feito com <span role="img" aria-label="coração">❤️</span> por LucasOdev<br/>
+        Direitos de imagem para Netflix<br/>
+        API usada do site Themoviedb.org<br/>
+      </footer>
+        {movieList.length <= 0 && 
+        <div className="loading">
+        <img src="https://user-images.githubusercontent.com/10602045/67531205-616ef880-f6ca-11e9-92ef-3a30af44d5c2.gif" ></img>
+      </div> }
+      
     </div>
   );
 }
